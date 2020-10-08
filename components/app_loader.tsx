c091aca75b08cc4539b552a10ef5dde6eb1bdd0a
classic-code <amos.daniel@fiddoo.com>
2020-10-08 04:40:31 +0100

    update

---
+++ components/app_loader.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { MoonLoader } from "react-spinners";
+
+export default function AppLoader({ runText }: { runText: string }) {
+  return (
+    <section className="AppLoader">
+      <div className="container">
+        <div className="logo" />
+        <p className="txt">{runText}</p>
+        <div className="loader">
+          <MoonLoader size="30px" color="#29DA45" />
+        </div>
+      </div>
+    </section>
+  );
+}
